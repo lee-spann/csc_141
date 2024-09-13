66800ed60de213296db05003ef31d67752907275
lee-spann <lee.spann001@albright.edu>
2024-09-12 23:44:36 -0400

    2.11 and 2.12

---
+++ 02_variables_and_data_types/02_11_adding_comments.py
@@ -0,0 +1,19 @@
+
+
+# 2-11 , this program prints my favorite number, Lee Spann, 9/5
+
+fav_num = 7
+
+message = f"My favorite number is {fav_num}."
+
+print(message)
+
+# 2-11 , this program involves mathematic problems that all have the result of 8, Lee Spann, 9/5
+
+print(5+3)
+
+print(11-3)
+
+print(4*2)
+
+print(16 // 2)
