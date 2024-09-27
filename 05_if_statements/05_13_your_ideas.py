93878f4b54db0155cee2608f466205808f6d30a0
lee-spann <lee.spann001@albright.edu>
2024-09-27 08:17:28 -0400

    spelling changed

--- 05_if_statements/05_13_your_ideas.py
+++ 05_if_statements/05_13_your_ideas.py
@@ -1,11 +1,11 @@
 
 
-# Some problems I could solve with my new found coding knowledge
+# Some problems I could solve with my new found python coding knowledge
 # is detection. In almost every game there is a type of detection
 # system, whether its with shooting games, fighting games, 
 # scary games, conditionals now gives me an idea on how I would 
 # code a game like those. Also lists, very helpful code, I can see
-# myself using it when it comes to keep scores, or anything really.
-# I can really feelm myself becoming a better coder after I complete 
+# myself using it when it comes to keeping scores, or anything really.
+# I can really feel myself becoming a better coder after I complete 
 # every assignment. I cant wait til the day where I out grown the book
 # and start doing way more complex problem solving and coding.
